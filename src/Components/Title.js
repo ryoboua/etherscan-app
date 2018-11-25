08f6456c0fde511f6d93d31d181f48a3ac7cb60b
Reggie <ryoboua@gmail.com>
2018-11-24 19:21:49 -0500

    Create Title.js

---
+++ src/Components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => (
+    <div style={{ 
+            width: '100%', 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center',
+            padding: '1em 0 0'
+        }}  >
+        <h1 style={{ textAlign: 'center', marginRight: '10px' }}>Ethereum Block Explorer</h1>
+        <img src="./Ethereum_logo_2014.svg" alt="Ethereum logo" width="50" height="100"/>
+    </div>
+)
