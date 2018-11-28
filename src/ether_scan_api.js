80cecc5ac0748d40a7396e3288d608f6df0c7f47
Reggie <ryoboua@gmail.com>
2018-11-27 23:51:44 -0500

    Update ether_scan_api.js

--- src/ether_scan_api.js
+++ src/ether_scan_api.js
@@ -1,7 +1,25 @@
-import etherScanApi from 'etherscan-api'
-
-export default etherScanApi.init(
-    process.env.REACT_APP_API_KEY,
-    process.env.REACT_APP_CHAIN,
-    process.env.REACT_APP_API_TIMEOUT,
+import etherScanApi from "etherscan-api"
+const api = etherScanApi.init(
+  process.env.REACT_APP_API_KEY,
+  process.env.REACT_APP_CHAIN,
+  process.env.REACT_APP_API_TIMEOUT
 )
+
+function getTxListPerPage(address, page = 1, offset = 20) {
+  return fetch(
+    `https://api.etherscan.io/api?
+module=account&
+action=txlist&
+address=${address}&
+startblock=0&
+endblock=latest&
+page=${page}&
+offset=${offset}&
+sort=desc&
+apikey=${process.env.REACT_APP_API_KEY}`
+  ).then(res => res.json())
+}
+
+api.account.getTxListPerPage = getTxListPerPage
+
+export default api
