fa60be0bf65a859bb1ee722500a92145004b4ef5
Reggie <ryoboua@gmail.com>
2018-11-27 23:51:30 -0500

    Update Title.js

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react"
 
 export default () => (
-    <div style={{ 
-            width: '100%', 
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center',
-            padding: '1em 0 0'
-        }}  >
-        <h1 style={{ textAlign: 'center', marginRight: '10px' }}>Ethereum Block Explorer</h1>
-        <img src="./Ethereum_logo_2014.svg" alt="Ethereum logo" width="50" height="100"/>
-    </div>
+  <div
+    style={{
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: "1em 0 0"
+    }}
+  >
+    {" "}
+                                              {/* 10px */}
+    <h1 style={{ textAlign: "center", marginRight: "0px" }}>
+      Ethereum Block Explorer
+    </h1>
+    {/* <img src="./Ethereum_logo_2014.svg" alt="Ethereum logo" width="50" height="100"/> */}
+  </div>
 )
