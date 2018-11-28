dc3b160b05f70cd25d259eedb4bb260c913050fc
Reggie <ryoboua@gmail.com>
2018-11-27 23:51:21 -0500

    Update AccountOverview.js

--- src/Components/Account/AccountOverview.js
+++ src/Components/Account/AccountOverview.js
@@ -1,40 +1,34 @@
-import React from 'react'
-import { Box, Grommet } from 'grommet'
-import { fromWei } from 'web3-utils'
-import theme from '../theme'
+import React from "react";
+import { Box, Grommet } from "grommet";
+import { fromWei, hexToNumberString } from "web3-utils";
+import theme from "../theme";
 
-export default ({ address, etherBalance, totalTransaction }) => {      
-    return (
-        <Grommet theme={theme}>
-            <Box
-                elevation="xlarge"
-                round="small"
-                responsive
-                width="800px"
-                margin="medium"
-                animation="fadeIn"
-                background={{
-                  color: 'white',
-                  opacity: 'medium'
-                }}
-            >
-              <div style={{padding: '0.5em 2em'}} >
-                <h3>
-                    Address: {address}
-                </h3>
-              </div>
-              <div style={{padding: '0.5em 2em'}} >
-                <h3>
-                    Balance: {fromWei(etherBalance, 'ether')}
-                </h3>
-              </div>
-  
-              <div style={{padding: '0.5em 2em'}} >
-                <h3>
-                    Transactions: {totalTransaction}
-                </h3>
-              </div>
-            </Box>
-        </Grommet>
+export default ({ address, etherBalance, totalTransaction }) => {
+  return (
+    <Grommet theme={theme}>
+      <Box
+        elevation="xlarge"
+        round="small"
+        responsive
+        width="800px"
+        margin="medium"
+        animation="fadeIn"
+        background={{
+          color: "white",
+          opacity: "medium"
+        }}
+      >
+        <div style={{ padding: "0.5em 2em" }}>
+          <h3>Address: {address}</h3>
+        </div>
+        <div style={{ padding: "0.5em 2em" }}>
+          <h3>Balance: {fromWei(etherBalance, "ether")}</h3>
+        </div>
 
-  )}
+        <div style={{ padding: "0.5em 2em" }}>
+          <h3>Sent Transactions: {hexToNumberString(totalTransaction)}</h3>
+        </div>
+      </Box>
+    </Grommet>
+  );
+};
