427b081eaedaa385b20707b9df9619b1473493df
Reggie <ryoboua@gmail.com>
2018-11-27 23:51:26 -0500

    Update Error.js

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -5,6 +5,15 @@ import { Share } from 'grommet-icons';
 export default ({ searchTerm, query }) => (
     <div style={{ width: '100%', textAlign: 'center', padding: '1em' }}>
         <h1 >Sorry, We Are Unable To Locate The {searchTerm}</h1>
-        <p>See if etherscan.io can load the {searchTerm}  <a href={`https://etherscan.io${query}`} target="_blank" rel="noopener noreferrer" ><Share /></a></p>
+        <p>
+            See if etherscan.io can load the {searchTerm}  
+            <a 
+                href={`https://etherscan.io${query}`} 
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                <Share />
+            </a>
+        </p>
     </div>
 )
