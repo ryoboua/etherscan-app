a86d68dcb83e5a365f7fac1024b4d3389d17502a
Reggie <ryoboua@gmail.com>
2018-11-27 23:51:14 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,63 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { Component } from "react"
+import { BrowserRouter as Router } from "react-router-dom"
 
-import ScrollTop from './Components/ScrollToTop'
-import SearchBar from './Components/SearchBar'
-import Title from './Components/Title'
-import { AccountRoute, BlockRoute, TxRoute } from './Routes/index'
+import ScrollTop from "./Components/ScrollToTop"
+import SearchBar from "./Components/SearchBar"
+import Title from "./Components/Title"
+import { AccountRoute, BlockRoute, TxRoute } from "./Routes/index"
 
-import client from './client'
+import client from "./client"
 
 class App extends Component {
   state = {
     accountData: {
       etherBalance: null,
       transactionList: null,
+      totalTransactionCount: null
     },
     blockData: null,
-    txData: null,
+    txData: null
   }
 
-  loadAccountData = address => client.fetchAccountData(address)
-                              .then(res => this.setState({ accountData: {...this.state.accountData, ...res} }))
-                  
-  loadBlockData = blockNumber => client.fetchBlockData(blockNumber)
-                                .then(blockData => this.setState({ blockData }))
+  loadAccountData = address =>
+    client
+      .fetchAccountData(address)
+      .then(res =>
+        this.setState({ accountData: { ...this.state.accountData, ...res } })
+      )
 
-  loadTxData = txHash => client.fetchTxData(txHash)
-                                .then(txData => this.setState({ txData }))
-                                    
+  loadBlockData = blockNumber =>
+    client
+      .fetchBlockData(blockNumber)
+      .then(blockData => this.setState({ blockData }))
+
+  loadTxData = txHash =>
+    client.fetchTxData(txHash).then(txData => this.setState({ txData }))
+
+  loadTransactionList = (address, page = 1) =>
+    client
+      .fetchAccountTransactionList(address, page)
+      .then(listOfNewTransactions => {
+        this.setState({
+          accountData: {
+            ...this.state.accountData,
+            transactionList: [
+              ...this.state.accountData.transactionList,
+              ...listOfNewTransactions
+            ]
+          }
+        })
+      })
+
   render() {
-    const { accountData, blockData, txData } = this.state
+    const {
+      state: { accountData, blockData, txData },
+      loadAccountData,
+      loadTransactionList,
+      loadBlockData,
+      loadTxData,
+      loadTotalTransactionCount
+    } = this
 
     return (
       <Router>
@@ -36,25 +65,26 @@ class App extends Component {
           <ScrollTop>
             <Title />
             <SearchBar />
-              <AccountRoute 
-                path="/address/:address" 
-                accountData={accountData}
-                loadAccountData={this.loadAccountData}
-              />
-              <BlockRoute 
-                path="/block/:blockNumber"
-                blockData={blockData}
-                loadBlockData={this.loadBlockData}
-              />
-              <TxRoute 
-                path="/tx/:txHash"
-                txData={txData}
-                loadTxData={this.loadTxData}
-              />
+            <AccountRoute
+              path="/address/:address"
+              accountData={accountData}
+              loadAccountData={loadAccountData}
+              loadTransactionList={loadTransactionList}
+            />
+            <BlockRoute
+              path="/block/:blockNumber"
+              blockData={blockData}
+              loadBlockData={loadBlockData}
+            />
+            <TxRoute
+              path="/tx/:txHash"
+              txData={txData}
+              loadTxData={loadTxData}
+            />
           </ScrollTop>
         </React.Fragment>
       </Router>
-    );
+    )
   }
 }
 
